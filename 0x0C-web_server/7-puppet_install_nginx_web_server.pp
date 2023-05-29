20be772dff65c1cadfab5e610f9798e62b37e203
Mohzis1 <113746411+Mohzis1@users.noreply.github.com>
2023-05-29 19:39:04 +0100

    Update 7-puppet_install_nginx_web_server.pphh

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ exec {'Home page':
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com/@mohzis\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:www.youtube.com/watch?v=QH2-TGUlwu4}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 

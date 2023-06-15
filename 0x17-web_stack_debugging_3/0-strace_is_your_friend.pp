294294c3c9a64892c255f377d2c1f06944fd6aaa
Mohzis1 <isangmoses0@gmail.com>
2023-06-15 05:06:50 -0700

    Debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#  This substitutes `.phpp` with `.php` in the file `wp-settings.php`.
+
+exec { 'replace-phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
